fb8bd5d30367b70a9dde99a5d01e0fdab4940d66
Anton Rutkevich <anton.rutkevich@gmail.com>
2015-11-20 12:00:30 +0300

    Integrated crashlytics

---
+++ app/src/main/java/com/rutkevich/cdworkshop/MainApp.java
@@ -0,0 +1,17 @@
+package com.rutkevich.cdworkshop;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
+    }
+}
